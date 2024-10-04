2df082d9c5b1ece14443ba9fc1aaddb539647f31
tranchaungoctri <tranchaungoctri@gmail.com>
2024-10-04 13:48:45 +0930

    commit

--- analysis.py
+++ analysis.py
@@ -72,7 +72,11 @@ def _parse_line(line):
 
 def _compute_stats():
     # ... you can add variables here ...
-    
+    n_triples = 0
+    n_people = set()
+    actor_movie_count ={}
+    actor_weight = {}
+    max_movies = 0
     # open file and read it line by line
     # assume utf8 encoding, ignore non-parseable characters
     with open(data_file, encoding="utf8", errors="ignore") as f:
@@ -88,7 +92,27 @@ def _compute_stats():
     # you can add print statements if you like, but only the
     # last four printed lines will be assessed;
     ###########################################################
-    
+            #increment number of triples
+            n_triples+= 1
+            #track district person
+            if  _is_uri(s) and s.startswith(uri_person):
+                n_people.add(s)
+            #count movies per actor
+            if p == predicate_has_actor:
+                actor = o
+                if _is_uri(actor) and actor.startswith(uri_person):
+                    if actor not in actor_movie_count:
+                        actor_movie_count[actor] = 0
+                    actor_movie_count[actor] += 1
+                    #track max movies that actor is in
+                    max_movies = max(max_movies, actor_movie_count[actor])
+            
+            #calculate actor weight
+            if p.startwith(predicate_prefix):
+                if p == predicate_has_actor:
+                    order = len(actor_weight) + 1
+                    actor_weight[o] = actor_weight.get(o, 0) + (1 / order)
+
     ###########################################################
     # n_triples -- number of distinct triples
     # n_people -- number of distinct people mentioned in ANY role
@@ -103,7 +127,17 @@ def _compute_stats():
     #               'highweight' is the largest cumulative weight.
     # s_name -- the name of the highweight actor
     ###########################################################
-    
+
+    #total of distinct people
+    n_people = len(n_people)
+
+    #total number of top actors
+    n_top_actors = sum(1 for count in actor_movie_count.values() if count == max_movies)
+
+    #calculate highweight actor and name
+    n_highweight_actor = max(actor_weight.values(), default=0)
+    s_name = max(actor_weight, key=actor_weight.get, default="")
+
     return n_triples, n_people, n_top_actors, n_highweight_actor, s_name
 
 # DO NOT CHANGE THE FINAL OUTPUT FORMATTING BELOW THIS LINE
